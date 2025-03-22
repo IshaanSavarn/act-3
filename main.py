e390dd1086f20dc99f7e7ee326398fb2f820bf5c
IshaanSavarn <isgamerishaan@gmail.com>
2025-03-22 20:06:07 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+def intro(name):
+    
+    print("He11o, Good morning! I am",name)
+morning! I am", name)
+print("I am here to help you with your queries Thank you",name)
+
+user_name = input("Enter your name")
+intro(user_name)
